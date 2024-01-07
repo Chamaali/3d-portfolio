c935474bed0d4af000dc147c462eea3445853db7
Ivan <imikon@proton.me>
2024-01-07 20:02:59 +0100

    paste house and sky models to Home page and added simple light

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import {useState} from 'react'
-import './App.css'
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import {About, Contacts, Home, Projects} from "./pages/index.js";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <main className='bg-slate-300/20'>
-        <Navbar/>
-        <Routes>
-            <Route path={'/'} element={<Home/>}/>
-            <Route path={'/about'} element={<About/>}/>
-            <Route path={'/projects'} element={<Projects/>}/>
-            <Route path={'/contacts'} element={<Contacts/>}/>
-        </Routes>
-    </main>
-  )
+    return (
+        <main className='bg-slate-300/20 w-full'>
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/about'} element={<About/>}/>
+                    <Route path={'/projects'} element={<Projects/>}/>
+                    <Route path={'/contacts'} element={<Contacts/>}/>
+                </Routes>
+            </BrowserRouter>
+        </main>
+    )
 }
 
 export default App
