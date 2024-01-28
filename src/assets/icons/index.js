33bd9a184709d851edfd520ef4911ade237e3da3
Ivan <imikon@proton.me>
2024-01-28 13:14:54 +0100

    build created

---
+++ src/assets/icons/index.js
@@ -0,0 +1,55 @@
+import css from './css.svg'
+import git from './git.svg'
+import github from './github.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import nextjs from './nextjs.svg'
+import react from './react.svg'
+import redux from './redux.svg'
+import sass from './sass.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import arrow from './arrow.svg'
+import soundon from './sound-on.svg'
+import soundoff from './sound-off-svgrepo-com.svg'
+import webpack from './webpack-icon.svg'
+import jest from './jest-svgrepo-com.svg'
+import threejs from './threejs-1.svg'
+import shadcn from './139895814.png'
+import cinema from './cinema-film-movie-svgrepo-com.svg'
+import computer from './computer-svgrepo-com.svg'
+import bachoo from './bachoo.svg'
+import computeradv from './computer-svgrepo-com_adv.svg'
+import crypto from './cryptoket.svg'
+import stack from './stack.svg'
+import film from './film-svgrepo-com.svg'
+
+export {
+    css,
+    git,
+    github,
+    html,
+    javascript,
+    nextjs,
+    react,
+    redux,
+    sass,
+    tailwindcss,
+    typescript,
+    linkedin,
+    arrow,
+    soundon,
+    soundoff,
+    webpack,
+    jest,
+    threejs,
+    shadcn,
+    cinema,
+    computer,
+    bachoo,
+    computeradv,
+    crypto,
+    stack,
+    film
+}
